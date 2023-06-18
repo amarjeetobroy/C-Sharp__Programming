cc7bcd513dd6135af4842c67e28a59a978bd1c14
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-18 17:13:15 +0530

    Variable

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace First{
+    class Program{
+        static void Main(string[]args){
+            int a = 10;
+            Console.WriteLine("The value of a = " +a);
+        }
+    }
+}
+
+/* Output
+ The value of a = 10
+ */

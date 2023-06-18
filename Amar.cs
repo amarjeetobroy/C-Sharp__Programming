362a53c1bfd37be628a80deb31d98b37f49828fe
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-18 20:14:06 +0530

    Input and Output

---
+++ Amar.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Amar
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter your name: ");
+        
+        string name = Console.ReadLine();
+        Console.Write("Your name is: "+name);
+    }
+}
+/* Output
+Enter your name: 
+Amarjeet kumar
+Your name is: Amarjeet kumar
+*/

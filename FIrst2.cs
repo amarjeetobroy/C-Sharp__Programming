3f0469f9e5ec3107221c864d39e81f71101b9d97
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-27 22:47:50 +0530

    Return statement

---
+++ FIrst2.cs
@@ -0,0 +1,15 @@
+using System;
+class First {
+  static void Main() {
+      int r =add(10,20);
+      Console.Write(r);
+ 
+  }
+  static int add(int x, int y){
+      return x+y;
+  }
+}
+/*
+Output
+30
+*/

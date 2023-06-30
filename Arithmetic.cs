a18d4b18607115dadfef05837624afcaba4a7a94
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-07-01 00:30:14 +0530

    Arithmetic Operator

---
+++ Arithmetic.cs
@@ -0,0 +1,16 @@
+using System;
+class Arithmetic {
+  static void Main() {
+    int a=100,b=50;
+    Console.WriteLine(a+b);
+    Console.WriteLine(a-b);
+    Console.WriteLine(a*b);
+    Console.WriteLine(a/b);
+  }
+}
+/* Output
+150
+50
+5000
+2
+*/

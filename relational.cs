bf4666f10e2d2cc929b937fa122949e5bab26a59
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-07-01 00:31:48 +0530

    Relational Operator

---
+++ relational.cs
@@ -0,0 +1,17 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    int a=100,b=50;
+    Console.WriteLine(a<b);
+    Console.WriteLine(a>b);
+    Console.WriteLine(a<=b);
+    Console.WriteLine(a!=b);
+  }
+}
+/* Output
+False
+True
+False
+True
+
+*/
